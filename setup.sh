b83d13a334d781e450e5e205a89849bd0bd1d311
David Hong <gdavid0510@gmail.com>
2021-07-14 15:00:43 +0900

    entries reordered

--- setup.sh
+++ setup.sh
@@ -17,27 +17,57 @@ sudo dpkg-reconfigure unattended-upgrades
 
 
 echo -e "\e[93mPurge useless packages\e[0m"
-sudo sed -i -E 's/.?.?.?archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 sudo apt purge -y thunderbird aisleriot gnome-mahjongg gnome-mines gnome-sudoku thunderbird transmission-common totem
 sudo apt autoremove -y
 
 
 
 echo -e "\e[93mAPT repository update\e[0m"
+sudo sed -i -E 's/.?.?.?archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 sudo apt update
 sudo apt upgrade -y
 
 
 
 echo -e "\e[93mInstalling CLI packages\e[0m"
-sudo apt install -y ncdu htop iputils-ping iputils-tracepath openssh-server byobu apt-transport-https ca-certificates curl gnupg lsb-release ffmpeg git perl whiptail lm_sensors
+sudo apt install -y ncdu htop iputils-ping iputils-tracepath openssh-server byobu apt-transport-https ca-certificates curl gnupg lsb-release ffmpeg git perl whiptail lm-sensors
 
 
 
 echo -e "\e[93mInstalling GUI packages\e[0m"
-sudo apt install -y baobab putty terminator filezilla fonts-noto-cjk* fonts-noto-mono fonts-noto-color-emoji gparted blueman
+sudo apt install -y baobab putty terminator filezilla fonts-noto-cjk* fonts-noto-mono fonts-noto-color-emoji gparted blueman fcitx-hangul
 echo -e "\e[92mMANUAL SETTINGS REQUIRED:\e[0m"
 echo -e "(terminator) profile settings. (putty) connection profiles."
+
+
+
+echo -e "\e[93mInstalling GNOME extensions requisities\e[0m"
+sudo apt install -y chrome-gnome-shell gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0
+echo -e "\e[92mMANUAL SETTINGS REQUIRED:\e[0m"
+echo "install firefox extension bridge."
+firefox -new-tab -url https://extensions.gnome.org/local/
+echo -e "\e[92mMANUAL SETTINGS REQUIRED:\e[0m"
+echo "install extensions."
+firefox -new-tab -url https://extensions.gnome.org/extension/15/alternatetab/ \
+ -new-tab -url https://extensions.gnome.org/extension/615/appindicator-support/ \
+ -new-tab -url https://extensions.gnome.org/extension/1071/applications-overview-tooltip/ \
+ -new-tab -url https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/ \
+ -new-tab -url https://extensions.gnome.org/extension/841/freon/ \
+ -new-tab -url https://extensions.gnome.org/extension/104/netspeed/ \
+ -new-tab -url https://extensions.gnome.org/extension/7/removable-drive-menu/ \
+ -new-tab -url https://extensions.gnome.org/extension/2741/remove-alttab-delay-v2/ \
+ -new-tab -url https://extensions.gnome.org/extension/906/sound-output-device-chooser/ \
+ -new-tab -url https://extensions.gnome.org/extension/355/status-area-horizontal-spacing/ \
+ -new-tab -url https://extensions.gnome.org/extension/1031/topicons/ \
+ -new-tab -url https://extensions.gnome.org/extension/1007/window-is-ready-notification-remover/ \
+ &
+
+
+echo -e "\e[93mInstalling grub-customizer\e[0m"
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
+sudo apt update
+sudo apt install -y grub-customizer
+sudo grub-customizer &
 
 
 
@@ -71,44 +101,16 @@ echo "fonts selection. theme selection."
 
 
 
-echo -e "\e[93mInstalling GNOME extensions requisities\e[0m"
-sudo apt install -y chrome-gnome-shell gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0
-echo -e "\e[92mMANUAL SETTINGS REQUIRED:\e[0m"
-echo "install extensions(Opening firefox)."
-firefox -new-tab -url https://extensions.gnome.org/local/ \
- -new-tab -url https://extensions.gnome.org/extension/15/alternatetab/ \
- -new-tab -url https://extensions.gnome.org/extension/615/appindicator-support/ \
- -new-tab -url https://extensions.gnome.org/extension/1071/applications-overview-tooltip/ \
- -new-tab -url https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/ \
- -new-tab -url https://extensions.gnome.org/extension/841/freon/ \
- -new-tab -url https://extensions.gnome.org/extension/104/netspeed/ \
- -new-tab -url https://extensions.gnome.org/extension/7/removable-drive-menu/ \
- -new-tab -url https://extensions.gnome.org/extension/2741/remove-alttab-delay-v2/ \
- -new-tab -url https://extensions.gnome.org/extension/906/sound-output-device-chooser/ \
- -new-tab -url https://extensions.gnome.org/extension/355/status-area-horizontal-spacing/ \
- -new-tab -url https://extensions.gnome.org/extension/1031/topicons/ \
- -new-tab -url https://extensions.gnome.org/extension/1007/window-is-ready-notification-remover/ \
- &
-
-
-echo -e "\e[93mInstalling grub-customizer\e[0m"
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
-sudo apt update
-sudo apt install -y grub-customizer
-
-
-
 echo -e "\e[93mInstalling media packages\e[0m"
-
-echo -e "\e[93m(VLC)\e[0m"
 sudo apt install -y vlc gimp inkscape
 
 
 
 ROS_VER=melodic
-ROS_WS=~/catkin_ws
+ROS_WS=$HOME/catkin_ws
 echo -e "\e[93mInstalling ROS version=$ROS_VER, workspace=$ROS_WS\e[0m"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential ros-$ROS_VER-desktop-full ros-$ROS_VER-turtlebot3* ros-$ROS_VER-gmapping
@@ -121,7 +123,7 @@ echo -e "\e[93mRestoring .bashrc with ROS configurations\e[0m"
 if [ -f ~/.bashrc ]; then
 	mv ~/.bashrc ~/.bashrc.bak
 fi
-wget ros.kasimov.synology.me/.bashrc -O ~/.bashrc
+wget https://ros.kasimov.synology.me/.bashrc -O ~/.bashrc
 source ~/.bashrc
 mkdir -p $ROS_WS/src
 cd $ROS_WS/src
@@ -138,11 +140,11 @@ sudo tar -xvf $ARDUINO_VER-linux64.tar.xz -C /opt/
 rm $ARDUINO_VER-linux64.tar.xz
 sudo /opt/$ARDUINO_VER/install.sh
 /opt/$ARDUINO_VER/arduino-linux-setup.sh $USER
-rm $HOME/Desktop/arduino*.desktop
+rm ~/Desktop/arduino*.desktop
 
 
 
-echo -e "\e[93mInstalling Virtualbox 6.1\e[0m"
+echo -e "\e[93mInstalling Virtualbox 6.1 repository\e[0m"
 echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 sudo apt update
@@ -152,4 +154,3 @@ sudo apt update
 
 
 echo -e "\e[92mInitlalization finished!"
-``

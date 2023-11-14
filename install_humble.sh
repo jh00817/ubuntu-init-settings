292d6b6fe2bd18055b1738eed2e23c43d92d7b41
jh <dreambait011@naver.com>
2023-11-14 17:57:39 +0900

    edit ros2

--- install_humble.sh
+++ install_humble.sh
@@ -44,82 +44,28 @@ sudo apt install -y -qq code
 
 ### ROBOTICS e-manual ros 설치
 
-# Apache License 2.0
-# Copyright (c) 2020, ROBOTIS CO., LTD.
+locale  # check for UTF-8
 
-echo "[Set the target OS, ROS version and name of catkin workspace]"
-name_os_version=${name_os_version:="focal"}
-name_ros_version=${name_ros_version:="humble"}
-name_catkin_workspace=${name_catkin_workspace:="ros2_ws"}
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
 
-echo "[Update the package lists]"
-sudo apt update -y
+locale  # verify settings
 
-echo "[Install build environment, the chrony, ntpdate and set the ntpdate]"
-sudo apt install -y chrony ntpdate curl build-essential
-sudo ntpdate ntp.ubuntu.com
+sudo apt -y install software-properties-common
+sudo add-apt-repository universe
 
-echo "[Add the ROS repository]"
-if [ ! -e /etc/apt/sources.list.d/ros-latest.list ]; then
-  sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu ${name_os_version} main\" > /etc/apt/sources.list.d/ros-latest.list"
-fi
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
-echo "[Download the ROS keys]"
-roskey=`apt-key list | grep "Open Robotics"`
-if [ -z "$roskey" ]; then
-  curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-fi
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-echo "[Check the ROS keys]"
-roskey=`apt-key list | grep "Open Robotics"`
-if [ -n "$roskey" ]; then
-  echo "[ROS key exists in the list]"
-else
-  echo "[Failed to receive the ROS key, aborts the installation]"
-  exit 0
-fi
-
-echo "[Update the package lists]"
-sudo apt update -y
-
-echo "[Install ros-desktop-full version of Noetic"
-sudo apt install -y ros-$name_ros_version-desktop-full
-
-echo "[Install RQT & Gazebo]"
-sudo apt install -y ros-$name_ros_version-rqt-* ros-$name_ros_version-gazebo-*
-
-echo "[Environment setup and getting rosinstall]"
-source /opt/ros/$name_ros_version/setup.sh
-sudo apt install -y python3-rosinstall python3-rosinstall-generator python3-wstool build-essential git
-
-echo "[Install rosdep and Update]"
-sudo apt install python3-rosdep
-
-echo "[Initialize rosdep and Update]"
-sudo sh -c "rosdep init"
-rosdep update
-
-echo "[Make the catkin workspace and test the catkin_make]"
-mkdir -p $HOME/$name_catkin_workspace/src
-cd $HOME/$name_catkin_workspace/src
-catkin_init_workspace
-cd $HOME/$name_catkin_workspace
-catkin_make
-
-echo "[Set the ROS evironment]"
-sh -c "echo \"alias eb='nano ~/.bashrc'\" >> ~/.bashrc"
-sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
-sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
-sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
-sh -c "echo \"alias cw='cd ~/$name_catkin_workspace'\" >> ~/.bashrc"
-sh -c "echo \"alias cs='cd ~/$name_catkin_workspace/src'\" >> ~/.bashrc"
-sh -c "echo \"alias cm='cd ~/$name_catkin_workspace && catkin_make'\" >> ~/.bashrc"
+sudo apt update
+sudo apt upgrade
+sudo apt install ros-humble-desktop
 
 sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
-sh -c "echo \"source ~/$name_catkin_workspace/devel/setup.bash\" >> ~/.bashrc"
-
-sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
-sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc"
 
 source $HOME/.bashrc
 
--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-cd $SCRIPT_LOCATION
+# email : dreambait011@naver.com
+# 안녕하세요, echo는 터미널에 문자를 출력하는 명령어 입니다. 주석 대신 echo 안에 쓰여진 내용들을 읽으시면 이해하기 쉬울 겁니다 !
 
-echo -e "\e[103m\e[30m  [ UBUNTU INIT SETTINGS HELPER ]  \e[0m"
-mkdir -p ~/applications
+
+
+echo ""
+echo "[Note] Target OS version  >>> Ubuntu 22.04.x (Focal Fossa)"
+echo "[Note] Target ROS version >>> ROS Humble"
+echo "[Note] Catkin workspace   >>> $HOME/ros2_ws"
+echo ""
+# echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
+# echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
+# read
+
 
 # mirror를 kakao로 변경 & 필요 없는 패키지 정리 & 업그레이드
 
@@ -14,46 +23,16 @@ echo 'APT::Periodic::Update-Package-Lists "0";' | sudo tee /etc/apt/apt.conf.d/2
 echo 'APT::Periodic::Unattended-Upgrade "0";' | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades
 sudo dpkg-reconfigure unattended-upgrades
 
-
-
 echo -e "\n\e[93mPurge useless packages\e[0m"
 sudo apt purge -y -qq thunderbird aisleriot gnome-mahjongg gnome-mines gnome-sudoku thunderbird transmission-common totem
 sudo apt autoremove -y -qq
-
-
 
 echo -e "\n\e[93mAPT repository update\e[0m"
 sudo sed -i -E 's/.?.?.?archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 sudo apt update -qq
 sudo apt upgrade -y -qq
 
-
-
-
-echo -e "\n\e[93mInstalling CLI packages\e[0m"
-sudo apt install -y -qq ncdu htop iputils-ping iputils-tracepath openssh-server byobu apt-transport-https ca-certificates curl gnupg lsb-release ffmpeg git perl whiptail lm-sensors bash dbus
-
-
-
-echo -e "\n\e[93mInstalling GUI packages\e[0m"
-sudo apt install -y -qq baobab putty terminator filezilla fonts-noto-cjk* fonts-noto-mono fonts-noto-color-emoji gparted blueman fcitx-hangul
-
-
-
-echo -e "\n\e[93mSetup language/input\e[0m"
-sudo apt install -y -qq $(check-language-support)
-gnome-language-selector
-
-
-
-echo -e "\n\e[93mInstalling grub-customizer\e[0m"
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
-sudo apt update -qq 
-sudo apt install -y -qq grub-customizer
-sudo grub-customizer &
-
-
-
+# VSCode 설치
 echo -e "\n\e[93mInstalling VSCode\e[0m"
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
@@ -63,38 +42,36 @@ sudo apt update -qq
 sudo apt install -y -qq code
 
 
+### ROBOTICS e-manual ros 설치
 
-echo -e "\n\e[93mInstalling Google Chrome\e[0m"
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-sudo apt update
-sudo apt install -y -qq google-chrome-stable
+locale  # check for UTF-8
 
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
 
+locale  # verify settings
 
-echo -e "\n\e[93mInstalling media packages\e[0m"
-sudo apt install -y -qq vlc inkscape
+sudo apt -y install software-properties-common
+sudo add-apt-repository universe
 
-
-
-ROS_VER=humble
-ROS_WS=$HOME/ros2_ws
-echo -e "\n\e[93mInstalling ROS version=$ROS_VER, workspace=$ROS_WS\e[0m"
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt update -qq
-sudo apt install -y -qq python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential ros-$ROS_VER-desktop-full ros-$ROS_VER-turtlebot3* ros-$ROS_VER-gmapping
-sudo rosdep init
-rosdep update
 
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
+sudo apt update
+sudo apt upgrade
+sudo apt install ros-humble-desktop
 
-echo -e "\n\e[93mRestoring .bashrc with ROS configurations\e[0m"
+sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
 
-echo -e "\n\e[93m YOU NEED COLCON_MAKE for your self. Editing the .bashrc part \e[0m"
+source $HOME/.bashrc
 
+###  ROS e-manual 설치 끝
 
+# 아두이노 설치
 ARDUINO_VER=arduino-1.8.15
 echo -e "\n\e[93mInstalling Arduino IDE version=$ARDUINO_VER\e[0m"
 wget https://downloads.arduino.cc/$ARDUINO_VER-linux64.tar.xz
@@ -105,6 +82,5 @@ sudo /opt/$ARDUINO_VER/install.sh
 rm ~/Desktop/arduino-arduinoide.desktop
 sudo apt autoremove -y -qq
 
-
-
-echo -e "\e[92mInitlalization finished!"
+echo "[Complete!!!]"
+exit 0
